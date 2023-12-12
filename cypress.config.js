f7f94de8f4b8895f2074755f52db431cc1f6cbca
Brijesh <bugbrijesh@gmail.com>
2023-12-12 13:28:32 +0530

    added

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'id3fq4', 
-  watchForFileChanges : false,
+  watchForFileChanges: false,
+  pageLoadTimeout: 12000,
   e2e: {
     baseUrl : "https://www.amazon.in/",
     setupNodeEvents(on, config) {
